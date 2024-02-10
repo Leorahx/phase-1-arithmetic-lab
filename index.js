fa0ffc79d52528fbf05bb75e7689ca4560aed8c1
Sarah Azad <aras.azad3@gmail.com>
2024-02-10 23:30:02 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
+
+let num3 = 104;
+let num4 = 25;
+let mod = num3 % num4;
